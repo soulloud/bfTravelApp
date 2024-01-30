700d6a78051705fc9ec567762afaf95e3cf0e8b8
HJK <zhaoxia9406@gmail.com>
2024-01-29 21:45:55 -0800

    [feature][Ui][3/n] Add new screen for search and destination details, and hock up with back navigation.

--- app/src/main/java/com/beaconfire/travel/navigation/Navigation.kt
+++ app/src/main/java/com/beaconfire/travel/navigation/Navigation.kt
@@ -2,10 +2,13 @@ package com.beaconfire.travel.navigation
 
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountBox
+import androidx.compose.material.icons.filled.ArrowBackIosNew
 import androidx.compose.material.icons.filled.Flight
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Login
 import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.Place
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.material.icons.filled.Settings
 import androidx.compose.ui.graphics.vector.ImageVector
 
@@ -14,22 +17,16 @@ sealed class Navigation(
     val title: String,
     val icon: ImageVector
 ) {
-    data object Login : Navigation(
-        route = "login",
-        title = "Login",
-        icon = Icons.Default.Login
+    data object Back : Navigation(
+        route = "back",
+        title = "Back",
+        icon = Icons.Default.ArrowBackIosNew
     )
 
-    data object Register : Navigation(
-        route = "register",
-        title = "Register",
-        icon = Icons.Default.AccountBox
-    )
-
-    data object Main : Navigation(
-        route = "main",
-        title = "Main",
-        icon = Icons.Default.Home
+    data object DestinationDetail : Navigation(
+        route = "destinationDetail",
+        title = "DestinationDetail",
+        icon = Icons.Default.Place
     )
 
     data object Home : Navigation(
@@ -38,10 +35,16 @@ sealed class Navigation(
         icon = Icons.Default.Home
     )
 
-    data object Trips : Navigation(
-        route = "trips",
-        title = "Trips",
-        icon = Icons.Default.Flight
+    data object Login : Navigation(
+        route = "login",
+        title = "Login",
+        icon = Icons.Default.Login
+    )
+
+    data object Main : Navigation(
+        route = "main",
+        title = "Main",
+        icon = Icons.Default.Home
     )
 
     data object Profile : Navigation(
@@ -50,9 +53,27 @@ sealed class Navigation(
         icon = Icons.Default.Person
     )
 
+    data object Register : Navigation(
+        route = "register",
+        title = "Register",
+        icon = Icons.Default.AccountBox
+    )
+
+    data object Search : Navigation(
+        route = "search",
+        title = "Search",
+        icon = Icons.Default.Search
+    )
+
     data object Settings : Navigation(
         route = "settings",
         title = "Settings",
         icon = Icons.Default.Settings
+    )
+
+    data object Trips : Navigation(
+        route = "trips",
+        title = "Trips",
+        icon = Icons.Default.Flight
     )
 }
