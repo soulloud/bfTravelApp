eac5b3b1785703155229aef238f1b9726ddf50f8
Soulloud <huichang@wustl.edu>
2024-01-30 20:36:01 -0500

    * push trip backend for debugging

--- app/src/main/java/com/beaconfire/travel/repo/model/Trip.kt
+++ app/src/main/java/com/beaconfire/travel/repo/model/Trip.kt
@@ -3,11 +3,11 @@ package com.beaconfire.travel.repo.model
 data class Trip(
     val tripId: String,
     val owner: String? = null,
-    val destinationList: List<Destination>? = null,
+    val destinationList: List<String>? = null, // list of destination ids
     val collaborators: List<String>? = null,
     val duration: String? = null,
     val title: String? = null,
     val description: String? = null,
-    val visibility: String? = null,
+    val visibility: String = "public",
     val numPeople: Long? = 1
 )
--- app/src/main/java/com/beaconfire/travel/trips/TripsScreen.kt
+++ app/src/main/java/com/beaconfire/travel/trips/TripsScreen.kt
@@ -1,9 +1,64 @@
 package com.beaconfire.travel.trips
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.beaconfire.travel.home.DestinationUiState
+import com.beaconfire.travel.repo.data.TripData
 
 @Composable
 fun TripsScreen() {
-    Text(text = "Trips")
+    val tripsViewModel: TripsViewModel = viewModel(factory = TripsViewModel.Factory)
+    val destinationUiState = tripsViewModel.destinationListUiState
+    val tripUiState = tripsViewModel.tripUiState
+
+
+//    // test for create new trip
+//    tripsViewModel.createNewTrip(
+//        TripData(
+//            owner = tripsViewModel.huichangId,
+//            title = "Go Japan"
+//            )
+//    )
+
+//    // test for delete trip
+//    if (tripUiState is TripUiState.LoadSucceed){
+//        for (trip in tripUiState.trips){
+//            if (trip.title.equals( "Go Japan")){
+//                tripsViewModel.deleteCurrentTrip(trip)
+//            }
+//        }
+//    }
+
+    // test for change visibility
+
+
+
+    //test for load trip
+    if (tripUiState is TripUiState.LoadSucceed){
+
+        val str = """
+            ${tripUiState.trips[0].title!!} : ${tripUiState.trips[0].destinationList} 
+            ${tripUiState.trips[1].title!!} : ${tripUiState.trips[1].destinationList} 
+        """.trimIndent()
+
+        Text(text = str, modifier = Modifier.padding(all = 30.dp))
+        tripsViewModel.getAllDestinationsInCurrentTrip(tripUiState.trips[0])
+    }
+
+    if (destinationUiState is DestinationUiState.LoadSucceed){
+        LazyColumn {
+            itemsIndexed(destinationUiState.destinations) { _, destination ->
+                val str = "${destination.name}, ${destination.location}"
+                Text(str, modifier = Modifier.padding(bottom = 20.dp))
+            }
+        }
+    }
+
+
 }
