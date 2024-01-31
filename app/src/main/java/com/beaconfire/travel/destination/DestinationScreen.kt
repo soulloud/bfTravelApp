19c831c1d5e92fbbb929eae9d9e2fbe9dff4eac3
Athena <zhaoxia9406@gmail.com>
2024-01-30 21:36:22 -0800

    [merge-conflict] Resolve merge conflict.

---
+++ app/src/main/java/com/beaconfire/travel/destination/DestinationScreen.kt
@@ -0,0 +1,9 @@
+package com.beaconfire.travel.destination
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun DestinationScreen() {
+
+
+}
--- app/src/main/java/com/beaconfire/travel/repo/DestinationRepository.kt
+++ app/src/main/java/com/beaconfire/travel/repo/DestinationRepository.kt
@@ -2,10 +2,11 @@ package com.beaconfire.travel.repo
 
 import android.graphics.BitmapFactory
 import com.beaconfire.travel.repo.data.DestinationData
-import com.beaconfire.travel.repo.model.Destination
-import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.ktx.Firebase
-import com.google.firebase.storage.ktx.storage
+import android.util.Log
+import com.beaconfire.travel.repo.model.Destination
+import com.beaconfire.travel.repo.model.Trip
+import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.callbackFlow
 import kotlinx.coroutines.flow.first
@@ -14,6 +15,33 @@ import kotlinx.coroutines.tasks.await
 class DestinationRepository {
 
     private val db = FirebaseFirestore.getInstance()
+
+    suspend fun getCurrentDestination(searchText: String): Destination = callbackFlow {
+        db.collection("destination")
+            .whereEqualTo("name", searchText)
+            .get()
+            .addOnSuccessListener { documents ->
+                val result = documents.first().toObject(Destination::class.java)
+                trySend(result)
+            }
+//            .addOnFailureListener{
+//                trySend()
+//            }
+            .await()
+        awaitClose()
+    }.first()
+
+    suspend fun getAllTrips(username: String): List<Trip> = callbackFlow{
+        db.collection("trip")
+            .whereEqualTo("owner", username)
+            .get()
+            .addOnSuccessListener { documents ->
+                val result = documents.toObjects(Trip::class.java)
+                trySend(result)
+            }
+            .await()
+        awaitClose()
+    }.first()
     private val storage = Firebase.storage
     private val storageUrl = "gs://beaconfiretripapp.appspot.com"
 
@@ -46,22 +74,19 @@ class DestinationRepository {
             .addOnFailureListener {
                 trySend(emptyList())
             }
-            .await()
-        awaitClose()
-    }.first()
 
-    suspend fun getImage() = callbackFlow {
-        // Points to the root reference
-        val storageRef = storage.reference
-        val MEGABYTE: Long = 1024 * 1024
+        suspend fun getImage() = callbackFlow {
+            // Points to the root reference
+            val storageRef = storage.reference
+            val MEGABYTE: Long = 1024 * 1024
 
-        val imagesRef = storageRef.child("newyork.jpg")
-        imagesRef.getBytes(MEGABYTE)
-            .addOnSuccessListener {bytes ->
-                val bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.size)
-                trySend(bitmap)
-            }
-            .await()
-        awaitClose()
-    }
+            val imagesRef = storageRef.child("newyork.jpg")
+            imagesRef.getBytes(MEGABYTE)
+                .addOnSuccessListener {bytes ->
+                    val bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.size)
+                    trySend(bitmap)
+                }
+                .await()
+            awaitClose()
+        }
 }
