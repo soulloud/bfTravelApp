c9f208644717c7a60fc105913145b3041de3f6c7
Athena <zhaoxia9406@gmail.com>
2024-01-31 23:01:26 -0800

    [feature][Ui][15/n] Download the image asset from firebase.

--- app/src/main/java/com/beaconfire/travel/repo/AssetRepository.kt
+++ app/src/main/java/com/beaconfire/travel/repo/AssetRepository.kt
@@ -2,17 +2,43 @@ package com.beaconfire.travel.repo
 
 import android.net.Uri
 import com.beaconfire.travel.AppContainer
+import com.google.firebase.storage.StorageReference
+import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.flow.callbackFlow
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.tasks.await
 import java.io.File
 
 class AssetRepository(private val appContainer: AppContainer) {
-    suspend fun uploadImageAsset(uri: Uri): String {
+    suspend fun uploadImageAsset(uri: Uri) = callbackFlow {
         val file = File(uri.toString())
-        val userId = appContainer.userRepository.getLoginUser()?.userId ?: "tmp"
         val timestamp = System.currentTimeMillis()
         val fileExt = file.path.substring(file.path.lastIndexOf(".") + 1)
-        val fileName = "assets/$userId/$timestamp.$fileExt"
-        val ref = appContainer.firebaseStorage.reference.child(fileName)
-        ref.putFile(uri)
-        return ref.downloadUrl.toString()
+        val filename = "$timestamp.$fileExt"
+        val storageRef = generateStorageReference(filename)
+        storageRef.putFile(uri)
+            .addOnSuccessListener { trySend(filename) }
+            .addOnFailureListener { trySend(null) }
+            .await()
+        awaitClose()
+    }.first()
+
+    suspend fun fetchImageAsset(filename: String) = callbackFlow {
+        generateStorageReference(filename)
+            .downloadUrl
+            .addOnSuccessListener { trySend(it) }
+            .addOnFailureListener { trySend(null) }
+            .await()
+        awaitClose()
+    }.first()
+
+    private suspend fun generateStorageReference(fileName: String): StorageReference {
+        val userId = appContainer.userRepository.getLoginUser()?.userId ?: "tmp"
+        val filename = "assets/$userId/$fileName"
+        return appContainer.firebaseStorage.reference.child(filename)
+    }
+
+    companion object {
+        val TAG = AssetRepository::class.java.simpleName
     }
 }
