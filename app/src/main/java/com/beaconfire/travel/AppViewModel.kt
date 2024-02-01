4af3c796cc759b0e61e91f71044e19c5c411144c
Athena <zhaoxia9406@gmail.com>
2024-02-01 14:00:42 -0800

    Merge branch 'athena/feature/ui' of github.com:soulloud/bfTravelApp into athena/feature/ui

--- app/src/main/java/com/beaconfire/travel/AppViewModel.kt
+++ app/src/main/java/com/beaconfire/travel/AppViewModel.kt
@@ -1,58 +1,29 @@
 package com.beaconfire.travel
 
-import android.util.Log
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.beaconfire.travel.navigation.Navigation
-import com.beaconfire.travel.repo.UserRepository
-import com.beaconfire.travel.repo.model.User
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.SharingStarted
-import kotlinx.coroutines.flow.combine
-import kotlinx.coroutines.flow.merge
-import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.update
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 
-class AppViewModel(
-    userRepository: UserRepository
-) : ViewModel() {
-    private val _currentScreen = MutableStateFlow<Navigation>(Navigation.Splash)
-    private val _currentUser = MutableStateFlow<User?>(null)
-    private val _user = merge(userRepository.loginUser, _currentUser)
-    val appUiModel = _user.combine(_currentScreen) { user, currentScreen ->
-        user?.let { _currentScreen.update { Navigation.Login } }
-        AppUiModel(user = user, currentScreen = currentScreen).also { Log.d(TAG, "$it") }
-    }.stateIn(
-        scope = viewModelScope,
-        started = SharingStarted.WhileSubscribed(),
-        initialValue = AppUiModel(null, Navigation.Splash)
-    )
-
-    init {
-        viewModelScope.launch {
-            withContext(Dispatchers.IO) {
-                _currentUser.update { userRepository.getLoginUser() }
-            }
-        }
-    }
+class AppViewModel : ViewModel() {
+    private val _appUiModel = MutableStateFlow(AppUiModel(Navigation.Splash))
+    val appUiModel: StateFlow<AppUiModel> = _appUiModel
 
     fun navigateTo(navigation: Navigation) {
         when (navigation) {
             Navigation.Splash -> {
-                _currentScreen.update { Navigation.Splash }
+                _appUiModel.update { it.copy(currentScreen = Navigation.Splash) }
             }
 
             Navigation.Login -> {
-                _currentScreen.update { Navigation.Login }
+                _appUiModel.update { it.copy(currentScreen = Navigation.Login) }
             }
 
             Navigation.Register -> {
-                _currentScreen.update { Navigation.Register }
+                _appUiModel.update { it.copy(currentScreen = Navigation.Register) }
             }
 
             else -> {}
@@ -63,7 +34,7 @@ class AppViewModel(
         private val TAG = AppViewModel::class.java.simpleName
         val Factory = viewModelFactory {
             initializer {
-                AppViewModel(mallApplication().container.userRepository)
+                AppViewModel()
             }
         }
     }
