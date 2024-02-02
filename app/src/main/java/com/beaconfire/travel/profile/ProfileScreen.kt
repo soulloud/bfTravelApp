b28d20ddb30ed5b06f18768416b54d45f8ae9683
HJK <zhaoxia9406@gmail.com>
2024-02-02 02:22:31 -0800

    [feature][ui][32/n] Update profile screen edit items.

--- app/src/main/java/com/beaconfire/travel/profile/ProfileScreen.kt
+++ app/src/main/java/com/beaconfire/travel/profile/ProfileScreen.kt
@@ -1,7 +1,6 @@
 package com.beaconfire.travel.profile
 
 import ProfileViewModel
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -18,8 +17,8 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Reviews
 import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
@@ -33,12 +32,9 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.lifecycle.viewmodel.compose.viewModel
-import coil.compose.rememberAsyncImagePainter
 import com.beaconfire.travel.ui.component.ProfileImage
 import com.beaconfire.travel.ui.component.button.CameraCaptureButton
 import com.beaconfire.travel.ui.component.button.GalleryPhotoPickerButton
@@ -159,13 +155,24 @@ fun ClickableTextWithDialog(
     var newValue by remember { mutableStateOf(value) }
 
     Column {
-        Text(
-            text = "$label: $newValue",
-            maxLines = maxLines,
-            modifier = Modifier.clickable {
-                showDialog = !showDialog
-            }
-        )
+        Row {
+            Text(
+                text = "$label:",
+                maxLines = maxLines,
+                modifier = Modifier
+                    .weight(1.0f)
+                    .padding(start = 16.dp)
+            )
+            Text(
+                text = newValue,
+                textAlign = TextAlign.End,
+                maxLines = maxLines,
+                modifier = Modifier
+                    .padding(end = 16.dp)
+                    .weight(1.0f)
+                    .clickable { showDialog = !showDialog }
+            )
+        }
 
         if (showDialog) {
             AlertDialog(
@@ -176,12 +183,9 @@ fun ClickableTextWithDialog(
                     Text(text = "Edit $label")
                 },
                 text = {
-
                     OutlinedTextField(
                         value = newValue,
-                        onValueChange = {
-                            newValue = it
-                        },
+                        onValueChange = { newValue = it },
                         label = { Text("New $label") },
                         maxLines = maxLines,
                         modifier = Modifier.padding(bottom = 8.dp)
@@ -208,31 +212,7 @@ fun ClickableTextWithDialog(
                 }
             )
         }
+
+        HorizontalDivider()
     }
 }
-
-
-@Preview(showBackground = true)
-@Composable
-fun ProfilePreview() {
-    MaterialTheme {
-        val profileViewModel = viewModel<ProfileViewModel>()
-        ProfileScreen(profileViewModel)
-    }
-}
-
-
-
-@Composable
-fun ImageLoader(url: String, modifier: Modifier = Modifier) {
-    val painter = rememberAsyncImagePainter(model = url)
-
-    Image(
-        painter = painter,
-        contentDescription = "Loaded image",
-        modifier = modifier,
-        contentScale = ContentScale.Crop // or other ContentScale as needed
-    )
-}
-
-
