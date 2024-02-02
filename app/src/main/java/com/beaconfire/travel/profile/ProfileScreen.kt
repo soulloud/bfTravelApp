ccf11533edb1e4ce1b1d593d562a6e3b1a05c469
HJK <63313765+ocean-tree11@users.noreply.github.com>
2024-02-01 17:27:56 -0800

    Temp (#22)
    * * updating profile ui
    
    * [feature][ui] merge conflict.
    
    ---------
    
    Co-authored-by: Soulloud <huichang@wustl.edu>

--- app/src/main/java/com/beaconfire/travel/profile/ProfileScreen.kt
+++ app/src/main/java/com/beaconfire/travel/profile/ProfileScreen.kt
@@ -4,6 +4,7 @@ import ProfileViewModel
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -15,14 +16,25 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountBox
 import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material.icons.filled.LocationOn
+import androidx.compose.material.icons.filled.Person
 import androidx.compose.material.icons.filled.Reviews
+import androidx.compose.material.icons.filled.Today
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -30,82 +42,106 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.platform.LocalUriHandler
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import coil.compose.rememberAsyncImagePainter
+import com.beaconfire.travel.repo.model.Profile
+import com.beaconfire.travel.ui.ReviewCard
+import com.beaconfire.travel.ui.component.ProfileImage
 import com.beaconfire.travel.ui.component.button.CameraCaptureButton
 import com.beaconfire.travel.ui.component.button.GalleryPhotoPickerButton
 
 @Composable
 fun ProfileScreen(profileViewModel: ProfileViewModel) {
     val profileUiModel by profileViewModel.profileUiModel.collectAsState()
+    val reviewUiModel by profileViewModel.reviewUiModel.collectAsState()
     var showEditProfile by remember { mutableStateOf(false) }
-    var showUploadedPhotos by remember { mutableStateOf(false) }
+    val showUploadedPhotos by remember { mutableStateOf(false) }
 
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .padding(16.dp),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
+            .padding(16.dp)
+            .padding(top = 30.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        ProfileItem("Edit Profile", Icons.Filled.Edit) { showEditProfile = !showEditProfile }
-        if (showEditProfile) {
-            LazyColumn(modifier = Modifier.fillMaxWidth()) {
-                profileUiModel.profile?.let { profile ->
-                    item {
-                        if (profileUiModel.capturedImageUri?.path?.isNotEmpty() == true) {
-                            Image(
-                                modifier = Modifier
-                                    .width(200.dp)
-                                    .height(200.dp)
-                                    .padding(16.dp, 8.dp),
-                                painter = rememberAsyncImagePainter(profileUiModel.capturedImageUri),
-                                contentDescription = null
-                            )
-                        }
-                        GalleryPhotoPickerButton { profileViewModel.onImageCaptured(it) }
-                        CameraCaptureButton { profileViewModel.onImageCaptured(it) }
-                        Button(onClick = { profileViewModel.setAsProfilePhoto() }) {
-                            Text(text = "Set as Profile Photo")
-                        }
-                        Text("Full Name: ${profile.fullName}", Modifier.padding(8.dp))
-                        Text("Location: ${profile.location}", Modifier.padding(8.dp))
-                        Text("About You: ${profile.aboutYou}", Modifier.padding(8.dp))
-                        Text("Join Date: ${profile.joinDate}", Modifier.padding(8.dp))
 
-                        // Button to show/hide uploaded photos
-                        Button(onClick = { showUploadedPhotos = !showUploadedPhotos }) {
-                            Text("Uploaded Photos")
-                        }
-                        // LazyColumn to display uploaded photos
-                        if (showUploadedPhotos) {
-                            LazyColumn {
-                                items(profile.uploadedPhotos) { photoUrl ->
-                                    ImageLoader(
-                                        url = photoUrl,
-                                        modifier = Modifier
-                                            .fillMaxWidth()
-                                            .height(200.dp)
-                                    )
-                                }
-                            }
-                        }
-                    }
+        profileUiModel.profile?.let { profile ->
+
+            val imageModifier = Modifier.size(120.dp).clip(CircleShape).clickable {}
+
+            if (profileUiModel.capturedImageUri?.path?.isNotEmpty() == true) {
+                Image(
+                    modifier = imageModifier,
+                    painter = rememberAsyncImagePainter(profileUiModel.capturedImageUri),
+                    contentDescription = null
+                )
+            }
+
+            GalleryPhotoPickerButton {
+                profileViewModel.onImageCaptured(it)
+                profileViewModel.setAsProfilePhoto()
+            }
+            CameraCaptureButton {
+                profileViewModel.onImageCaptured(it)
+                profileViewModel.setAsProfilePhoto()
+            }
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            ClickableTextWithDialog(
+                label = "Display Name",
+                value = profile.fullName,
+                profileViewModel = profileViewModel
+            )
+
+            Spacer(modifier = Modifier.height(8.dp))
+
+            ClickableTextWithDialog(
+                label = "Location",
+                value = profile.location,
+                profileViewModel = profileViewModel
+            )
+
+            Spacer(modifier = Modifier.height(8.dp))
+
+            ClickableTextWithDialog(
+                label = "Introduction",
+                value = profile.aboutYou,
+                maxLines = 5,
+                profileViewModel = profileViewModel
+            )
+
+            Spacer(modifier = Modifier.height(8.dp))
+
+            ClickableTextWithDialog(
+                label = "Join Date",
+                value = profile.joinDate,
+                profileViewModel = profileViewModel
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            ProfileItem("Your reviews", Icons.Filled.Reviews, onClick = {})
+
+            LazyColumn(
+                modifier = Modifier.padding(bottom = 70.dp)
+            ) {
+                items(reviewUiModel.reviews) {review ->
+                    ReviewCard(review = review)
                 }
             }
-        }
-        ProfileItem("Your reviews", Icons.Filled.Reviews, onClick = {})
-        Spacer(modifier = Modifier.height(128.dp))
-        Button(
-            onClick = { /* Handle log out */ },
-            modifier = Modifier.fillMaxWidth()
-        ) {
-            Text("Delete Account")
         }
     }
 }
@@ -125,6 +161,70 @@ fun ProfileItem(text: String, iconId: ImageVector, onClick: () -> Unit) {
     }
 }
 
+@Composable
+fun ClickableTextWithDialog(
+    label: String,
+    value: String,
+    maxLines: Int = 1,
+    profileViewModel: ProfileViewModel
+) {
+    var showDialog by remember { mutableStateOf(false) }
+    var newValue by remember { mutableStateOf(value) }
+
+    Column {
+        Text(
+            text = "$label: $newValue",
+            maxLines = maxLines,
+            modifier = Modifier.clickable {
+                showDialog = !showDialog
+            }
+        )
+
+        if (showDialog) {
+            AlertDialog(
+                onDismissRequest = {
+                    showDialog = false
+                },
+                title = {
+                    Text(text = "Edit $label")
+                },
+                text = {
+
+                    OutlinedTextField(
+                        value = newValue,
+                        onValueChange = {
+                            newValue = it
+                        },
+                        label = { Text("New $label") },
+                        maxLines = maxLines,
+                        modifier = Modifier.padding(bottom = 8.dp)
+                    )
+                },
+                confirmButton = {
+                    TextButton(
+                        onClick = {
+                            showDialog = false
+                            profileViewModel.updateName(newValue)
+                        }
+                    ) {
+                        Text(text = "Save")
+                    }
+                },
+                dismissButton = {
+                    TextButton(
+                        onClick = {
+                            showDialog = false
+                        }
+                    ) {
+                        Text(text = "Cancel")
+                    }
+                }
+            )
+        }
+    }
+}
+
+
 @Preview(showBackground = true)
 @Composable
 fun ProfilePreview() {
@@ -133,6 +233,8 @@ fun ProfilePreview() {
         ProfileScreen(profileViewModel)
     }
 }
+
+
 
 @Composable
 fun ImageLoader(url: String, modifier: Modifier = Modifier) {
@@ -145,3 +247,5 @@ fun ImageLoader(url: String, modifier: Modifier = Modifier) {
         contentScale = ContentScale.Crop // or other ContentScale as needed
     )
 }
+
+
